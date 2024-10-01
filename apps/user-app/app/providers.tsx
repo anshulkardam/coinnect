13c8582d5058fdc3b86e6115f8bbd6d844b6b134
anshulkardam <anshulkardamsr@gmail.com>
2024-10-01 23:57:48 +0530

    boilerplate

--- apps/user-app/app/providers.tsx
+++ apps/user-app/app/providers.tsx
@@ -3,6 +3,7 @@
 import { ReactNode } from 'react';
 import { Provider as ReduxProvider } from 'react-redux';
 import { store } from 'store/shared-store'; // Import your shared Redux store
+import { SessionProvider } from "next-auth/react";
 
 interface ProvidersProps {
   children: ReactNode;
@@ -11,8 +12,10 @@ interface ProvidersProps {
 const Providers = ({ children }: ProvidersProps) => {
   return (
     <ReduxProvider store={store}>
+        <SessionProvider>
       {/* Add other providers here (e.g., ThemeProvider) */}
       {children}
+      </SessionProvider>
     </ReduxProvider>
   );
 };
